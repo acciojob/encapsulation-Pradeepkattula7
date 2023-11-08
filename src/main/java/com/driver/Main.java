8a981e458e9aec5e30e11d43ef967bfb2ac0017c
Pradeep Kattula <pradeepkattula390@gmail.com>
2023-11-08 11:47:58 +0530

    changes made1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly obj =new RWOnly();
+
+        //obj.name="pradeep";
+
+        //System.out.println(obj.name);
+
+        obj.setName("pradeep");
+
+        System.out.println(obj.getName());
+    }
 }
